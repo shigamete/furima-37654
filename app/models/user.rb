371fa2e8da9914fa8d6c930e6a57420b6fb87687
shigamete <daiki_02@ymail.ne.jp>
2022-03-31 02:34:49 +0900

    [全角ひらがな/カタカナ/漢字]&[全角カタカナ]バリデーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,9 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname,       presence: true
-  validates :lastname_full,  presence: true
-  validates :firstname_full, presence: true
-  validates :lastname_kana,  presence: true
-  validates :firstname_kana, presence: true
+  validates :lastname_full,  presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+  validates :firstname_full, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+  validates :lastname_kana,  presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
+  validates :firstname_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
   validates :birthday,       presence: true
 end
