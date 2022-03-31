343a536b9f79acd29dff04e648de2e92b6fa101d
shigamete <daiki_02@ymail.ne.jp>
2022-03-31 10:28:11 +0900

    エラーハンドリング表示修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,9 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname,       presence: true
-  validates :lastname_full,  presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
-  validates :firstname_full, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
-  validates :lastname_kana,  presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
-  validates :firstname_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
+  validates :lastname_full,  presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid. Input full-width characters" }
+  validates :firstname_full, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid. Input full-width characters"}
+  validates :lastname_kana,  presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width katakana characters"}
+  validates :firstname_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width katakana characters"}
   validates :birthday,       presence: true
 end
