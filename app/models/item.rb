564419b0633c227c7d0a9ec35cda3a76e8ae1b6d
shigamete <daiki_02@ymail.ne.jp>
2022-04-01 13:29:39 +0900

    Itemモデルのバリデーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,7 @@
 class Item < ApplicationRecord
+  validates :name,   presence: true
+  validates :explan, presence: true
+  validates :price,  presence: true, numericality: {only_integer: true, greater_than_or_equal_to: 300,less_than_or_equal_to: 9999999}
+  
   belongs_to :user
 end
