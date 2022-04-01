953693fb8e71ce7d81e71424f9ad2021bad3c7e7
shigamete <daiki_02@ymail.ne.jp>
2022-04-01 19:16:08 +0900

    ActiveHash:カテゴリー追記

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,9 +1,10 @@
 class Item < ApplicationRecord
-  validates :image,  presence: true
-  validates :name,   presence: true
-  validates :explan, presence: true
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  validates :image, :name, :explan, presence: true
+  validates :category_id, numericality: { other_than: 1, message: "can't be blank"}
   validates :price,  presence: true, numericality: {only_integer: true, greater_than_or_equal_to: 300,less_than_or_equal_to: 9999999}, format: /\A[0-9]+\z/
   
   belongs_to :user
   has_one_attached :image
+  belongs_to_active_hash :category  
 end
