278df2b45066d4d29e327599e5ad3f60f6c2ec8b
shigamete <daiki_02@ymail.ne.jp>
2022-04-01 20:30:15 +0900

    ActiveHash:商品の状態追記

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,10 +1,11 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   validates :image, :name, :explan, presence: true
-  validates :category_id, numericality: { other_than: 1, message: "can't be blank"}
+  validates :category_id, :status_id, numericality: { other_than: 1, message: "can't be blank"}
   validates :price,  presence: true, numericality: {only_integer: true, greater_than_or_equal_to: 300,less_than_or_equal_to: 9999999}, format: /\A[0-9]+\z/
   
   belongs_to :user
+  belongs_to :category
+  belongs_to :status
   has_one_attached :image
-  belongs_to_active_hash :category  
 end
