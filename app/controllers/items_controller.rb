7f4fd7209fc5d633876c49307ba46abf3fadd6f2
shigamete <daiki_02@ymail.ne.jp>
2022-04-01 23:05:16 +0900

    入力微修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -17,6 +17,6 @@ class ItemsController < ApplicationController
 
 private
   def item_params
-    params.require(:item).permit(:image, :neme, :explan, :category_id, :status_id, :shippingfee_payer_id, :prefecture_id, :shipping_day_id, :price).merge(user_id:current_user.id)
+    params.require(:item).permit(:image, :name, :explan, :category_id, :status_id, :shippingfee_payer_id, :prefecture_id, :shipping_day_id, :price).merge(user_id:current_user.id)
   end
 end
