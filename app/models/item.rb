1d55fa8c2c463b4ef90d99bf680f6e3be77b7573
shigamete <daiki_02@ymail.ne.jp>
2022-04-01 22:37:41 +0900

    ActiveHash:発送までの日数追記

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,7 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   validates :image, :name, :explan, presence: true
-  validates :category_id, :status_id, :shippingfee_payer_id, :prefecture_id, numericality: { other_than: 1, message: "can't be blank"}
+  validates :category_id, :status_id, :shippingfee_payer_id, :prefecture_id, :shipping_day_id, numericality: { other_than: 1, message: "can't be blank"}
   validates :price,  presence: true, numericality: {only_integer: true, greater_than_or_equal_to: 300,less_than_or_equal_to: 9999999}, format: /\A[0-9]+\z/
   
   belongs_to :user
@@ -9,5 +9,6 @@ class Item < ApplicationRecord
   belongs_to :status
   belongs_to :shippingfee_payer
   belongs_to :prefecture
+  belongs_to :shipping_day
   has_one_attached :image
 end
