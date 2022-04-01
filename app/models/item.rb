3845516ef1fa5e094205bf02e7783d4bbabc34f6
shigamete <daiki_02@ymail.ne.jp>
2022-04-01 22:12:25 +0900

    ActiveHash:配送料の負担追記

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,11 +1,12 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   validates :image, :name, :explan, presence: true
-  validates :category_id, :status_id, numericality: { other_than: 1, message: "can't be blank"}
+  validates :category_id, :status_id, :shippingfee_payer_id, numericality: { other_than: 1, message: "can't be blank"}
   validates :price,  presence: true, numericality: {only_integer: true, greater_than_or_equal_to: 300,less_than_or_equal_to: 9999999}, format: /\A[0-9]+\z/
   
   belongs_to :user
   belongs_to :category
   belongs_to :status
+  belongs_to :shippingfee_payer
   has_one_attached :image
 end
