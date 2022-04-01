e3e5eab53aa6af67c7a107639ba5e08cddeebe29
shigamete <daiki_02@ymail.ne.jp>
2022-04-01 18:03:45 +0900

    画像、金額バリデーション追記

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,8 @@
 class Item < ApplicationRecord
+  validates :image,  presence: true
   validates :name,   presence: true
   validates :explan, presence: true
-  validates :price,  presence: true, numericality: {only_integer: true, greater_than_or_equal_to: 300,less_than_or_equal_to: 9999999}
+  validates :price,  presence: true, numericality: {only_integer: true, greater_than_or_equal_to: 300,less_than_or_equal_to: 9999999}, format: /\A[0-9]+\z/
   
   belongs_to :user
   has_one_attached :image
