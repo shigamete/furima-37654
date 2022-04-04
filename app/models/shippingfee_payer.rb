67fc4322e13dd6ae149bd91ccd3952f62244aa32
shigamete <daiki_02@ymail.ne.jp>
2022-04-04 12:43:18 +0900

    Rubocop自動修正(商品出品機能)

--- app/models/shippingfee_payer.rb
+++ app/models/shippingfee_payer.rb
@@ -5,6 +5,6 @@ class ShippingfeePayer < ActiveHash::Base
     { id: 3, name: '送料込み(出品者負担)' }
   ]
 
-    include ActiveHash::Associations
-    has_many :items
-  end
+  include ActiveHash::Associations
+  has_many :items
+end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -7,14 +7,14 @@ RSpec.describe Item, type: :model do
 
   describe '商品出品' do
     context '商品が出品できる場合' do
-      it 'image,name,explan,category_id,status_id,shippingfee_payer_id,prefecture_id,shipping_day_id,priceが存在していれば出品できる'do
+      it 'image,name,explan,category_id,status_id,shippingfee_payer_id,prefecture_id,shipping_day_id,priceが存在していれば出品できる' do
         expect(@item).to be_valid
       end
     end
     context '商品が出品できない場合' do
       it 'imageが空では出品できない' do
         @item.image = nil
-        @item.valid?        
+        @item.valid?
         expect(@item.errors.full_messages).to include("Image can't be blank")
       end
       it 'nameが空では出品できない' do
@@ -60,22 +60,22 @@ RSpec.describe Item, type: :model do
       it 'Userが紐付いていないと出品できない' do
         @item.user = nil
         @item.valid?
-        expect(@item.errors.full_messages).to include("User must exist")
+        expect(@item.errors.full_messages).to include('User must exist')
       end
       it 'priceに半角数字以外が含まれている場合は出品できない' do
         @item.price = '10000あ'
         @item.valid?
-        expect(@item.errors.full_messages).to include("Price is not a number")
+        expect(@item.errors.full_messages).to include('Price is not a number')
       end
       it 'priceが300円未満では出品できない' do
         @item.price = '299'
         @item.valid?
-        expect(@item.errors.full_messages).to include("Price must be greater than or equal to 300")
+        expect(@item.errors.full_messages).to include('Price must be greater than or equal to 300')
       end
       it ' priceが9999999円を超えると出品できない' do
         @item.price = '10000000'
         @item.valid?
-        expect(@item.errors.full_messages).to include("Price must be less than or equal to 9999999")
+        expect(@item.errors.full_messages).to include('Price must be less than or equal to 9999999')
       end
       it 'category_idが未選択(id = 1)では出品できない' do
         @item.category_id = 1
